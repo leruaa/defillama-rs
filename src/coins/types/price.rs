71c9efd3023273d412bd6ff2c10f61b36c45c3e4
leruaa <aurelien.catinon@gmail.com>
2023-11-03 12:43:56 +0100

    Impl Price to Price conversion

--- src/coins/types/price.rs
+++ src/coins/types/price.rs
@@ -1,3 +1,5 @@
+use anyhow::Error;
+use dashu_float::{round::mode::HalfAway, DBig, FBig};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -7,4 +9,14 @@ pub struct Price {
     pub price: f64,
     pub timestamp: u64,
     pub confidence: f64,
+}
+
+impl TryFrom<Price> for DBig {
+    type Error = Error;
+
+    fn try_from(value: Price) -> Result<Self, Self::Error> {
+        FBig::<HalfAway, 2>::try_from(value.price)
+            .map(|d| d.with_base::<10>().value())
+            .map_err(Into::into)
+    }
 }
